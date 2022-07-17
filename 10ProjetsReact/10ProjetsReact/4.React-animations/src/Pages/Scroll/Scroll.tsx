71ef0a3e418c1286f48c022391d1332f6489197a
Julien GOLETTO <julien.goletto@gmail.com>
2022-07-17 14:54:23 +0200

    fin des essais avec react spring et react-waypoint

--- 10ProjetsReact/10ProjetsReact/4.React-animations/src/Pages/Scroll/Scroll.tsx
+++ 10ProjetsReact/10ProjetsReact/4.React-animations/src/Pages/Scroll/Scroll.tsx
@@ -1,7 +1,152 @@
-import './Scroll.css';
+import { useState } from "react";
+import { Waypoint } from "react-waypoint";
+import { useSpring, animated } from "react-spring";
+import "./Scroll.css";
 
 export default function Scroll() {
+  const [toggleTxt, setToggleTxt] = useState(false);
+  const animation = useSpring({
+    opacity: toggleTxt? 1 : 0,
+    transform: toggleTxt ? "translateX(0)" : "translateX(-50%)",
+  })
   return (
-    <div>Scroll</div>
-  )
+    <div>
+      <p className="scroll-txt">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+        voluptatibus, hic doloribus at rem consequuntur facere similique
+        quisquam odit libero quae, pariatur mollitia dolor rerum consequatur
+        voluptatem dolores odio sed! Quas esse quidem eveniet vitae, totam nemo
+        perspiciatis ipsam nostrum non. Incidunt ad facere velit unde
+        voluptates. Numquam autem saepe id ad, a voluptas eum aperiam,
+        voluptatum praesentium accusamus nisi molestiae aspernatur nobis ea
+        odit? Voluptatem, dicta recusandae. Necessitatibus deleniti sed nesciunt
+        odio iure dolorem, aut nulla aspernatur eaque ducimus dolor quam ratione
+        amet omnis. Rerum quaerat omnis voluptatibus asperiores, dicta dolorum.
+        Dolor amet, dignissimos quibusdam odit voluptatum sed quae molestiae nam
+        voluptate nemo. Maiores, recusandae. Sunt quidem dolorem perspiciatis
+        ratione accusantium, iure voluptatibus aut itaque saepe aliquid
+        consequatur beatae quod dolore in necessitatibus fugit hic tempora
+        accusamus, magni minus assumenda illum non! Culpa eaque officia totam
+        ipsam, nesciunt sapiente veritatis, non aperiam molestias consequatur
+        dignissimos sint! Repellendus magni, pariatur quibusdam, praesentium
+        molestiae veniam maxime cupiditate qui fuga ducimus sint nostrum id nam
+        eius nisi alias nulla enim consequatur assumenda obcaecati modi
+        architecto soluta perferendis? Cumque facere nobis provident modi
+        veritatis illum nemo similique, necessitatibus veniam dolores fugiat
+        officia! Ullam ea cum adipisci, quae nihil neque illum ducimus nisi.
+        Exercitationem.
+      </p>
+      <p className="scroll-txt">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+        voluptatibus, hic doloribus at rem consequuntur facere similique
+        quisquam odit libero quae, pariatur mollitia dolor rerum consequatur
+        voluptatem dolores odio sed! Quas esse quidem eveniet vitae, totam nemo
+        perspiciatis ipsam nostrum non. Incidunt ad facere velit unde
+        voluptates. Numquam autem saepe id ad, a voluptas eum aperiam,
+        voluptatum praesentium accusamus nisi molestiae aspernatur nobis ea
+        odit? Voluptatem, dicta recusandae. Necessitatibus deleniti sed nesciunt
+        odio iure dolorem, aut nulla aspernatur eaque ducimus dolor quam ratione
+        amet omnis. Rerum quaerat omnis voluptatibus asperiores, dicta dolorum.
+        Dolor amet, dignissimos quibusdam odit voluptatum sed quae molestiae nam
+        voluptate nemo. Maiores, recusandae. Sunt quidem dolorem perspiciatis
+        ratione accusantium, iure voluptatibus aut itaque saepe aliquid
+        consequatur beatae quod dolore in necessitatibus fugit hic tempora
+        accusamus, magni minus assumenda illum non! Culpa eaque officia totam
+        ipsam, nesciunt sapiente veritatis, non aperiam molestias consequatur
+        dignissimos sint! Repellendus magni, pariatur quibusdam, praesentium
+        molestiae veniam maxime cupiditate qui fuga ducimus sint nostrum id nam
+        eius nisi alias nulla enim consequatur assumenda obcaecati modi
+        architecto soluta perferendis? Cumque facere nobis provident modi
+        veritatis illum nemo similique, necessitatibus veniam dolores fugiat
+        officia! Ullam ea cum adipisci, quae nihil neque illum ducimus nisi.
+        Exercitationem.
+      </p>
+      < Waypoint
+      bottomOffset="30%"
+        onEnter={() => (!toggleTxt) && setToggleTxt(true)}
+      />
+      < animated.div style={animation} className="cta-section">
+      <h2>N'en râtez pas une miette !</h2>
+      <form>
+        <label htmlFor="email">Inscrivez-vous à la newsletter</label>
+        <input type="email" id="email" />
+      </form>
+      </animated.div>
+      <p className="scroll-txt">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+        voluptatibus, hic doloribus at rem consequuntur facere similique
+        quisquam odit libero quae, pariatur mollitia dolor rerum consequatur
+        voluptatem dolores odio sed! Quas esse quidem eveniet vitae, totam nemo
+        perspiciatis ipsam nostrum non. Incidunt ad facere velit unde
+        voluptates. Numquam autem saepe id ad, a voluptas eum aperiam,
+        voluptatum praesentium accusamus nisi molestiae aspernatur nobis ea
+        odit? Voluptatem, dicta recusandae. Necessitatibus deleniti sed nesciunt
+        odio iure dolorem, aut nulla aspernatur eaque ducimus dolor quam ratione
+        amet omnis. Rerum quaerat omnis voluptatibus asperiores, dicta dolorum.
+        Dolor amet, dignissimos quibusdam odit voluptatum sed quae molestiae nam
+        voluptate nemo. Maiores, recusandae. Sunt quidem dolorem perspiciatis
+        ratione accusantium, iure voluptatibus aut itaque saepe aliquid
+        consequatur beatae quod dolore in necessitatibus fugit hic tempora
+        accusamus, magni minus assumenda illum non! Culpa eaque officia totam
+        ipsam, nesciunt sapiente veritatis, non aperiam molestias consequatur
+        dignissimos sint! Repellendus magni, pariatur quibusdam, praesentium
+        molestiae veniam maxime cupiditate qui fuga ducimus sint nostrum id nam
+        eius nisi alias nulla enim consequatur assumenda obcaecati modi
+        architecto soluta perferendis? Cumque facere nobis provident modi
+        veritatis illum nemo similique, necessitatibus veniam dolores fugiat
+        officia! Ullam ea cum adipisci, quae nihil neque illum ducimus nisi.
+        Exercitationem.
+      </p>
+      <p className="scroll-txt">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+        voluptatibus, hic doloribus at rem consequuntur facere similique
+        quisquam odit libero quae, pariatur mollitia dolor rerum consequatur
+        voluptatem dolores odio sed! Quas esse quidem eveniet vitae, totam nemo
+        perspiciatis ipsam nostrum non. Incidunt ad facere velit unde
+        voluptates. Numquam autem saepe id ad, a voluptas eum aperiam,
+        voluptatum praesentium accusamus nisi molestiae aspernatur nobis ea
+        odit? Voluptatem, dicta recusandae. Necessitatibus deleniti sed nesciunt
+        odio iure dolorem, aut nulla aspernatur eaque ducimus dolor quam ratione
+        amet omnis. Rerum quaerat omnis voluptatibus asperiores, dicta dolorum.
+        Dolor amet, dignissimos quibusdam odit voluptatum sed quae molestiae nam
+        voluptate nemo. Maiores, recusandae. Sunt quidem dolorem perspiciatis
+        ratione accusantium, iure voluptatibus aut itaque saepe aliquid
+        consequatur beatae quod dolore in necessitatibus fugit hic tempora
+        accusamus, magni minus assumenda illum non! Culpa eaque officia totam
+        ipsam, nesciunt sapiente veritatis, non aperiam molestias consequatur
+        dignissimos sint! Repellendus magni, pariatur quibusdam, praesentium
+        molestiae veniam maxime cupiditate qui fuga ducimus sint nostrum id nam
+        eius nisi alias nulla enim consequatur assumenda obcaecati modi
+        architecto soluta perferendis? Cumque facere nobis provident modi
+        veritatis illum nemo similique, necessitatibus veniam dolores fugiat
+        officia! Ullam ea cum adipisci, quae nihil neque illum ducimus nisi.
+        Exercitationem.
+      </p>
+      <p className="scroll-txt">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+        voluptatibus, hic doloribus at rem consequuntur facere similique
+        quisquam odit libero quae, pariatur mollitia dolor rerum consequatur
+        voluptatem dolores odio sed! Quas esse quidem eveniet vitae, totam nemo
+        perspiciatis ipsam nostrum non. Incidunt ad facere velit unde
+        voluptates. Numquam autem saepe id ad, a voluptas eum aperiam,
+        voluptatum praesentium accusamus nisi molestiae aspernatur nobis ea
+        odit? Voluptatem, dicta recusandae. Necessitatibus deleniti sed nesciunt
+        odio iure dolorem, aut nulla aspernatur eaque ducimus dolor quam ratione
+        amet omnis. Rerum quaerat omnis voluptatibus asperiores, dicta dolorum.
+        Dolor amet, dignissimos quibusdam odit voluptatum sed quae molestiae nam
+        voluptate nemo. Maiores, recusandae. Sunt quidem dolorem perspiciatis
+        ratione accusantium, iure voluptatibus aut itaque saepe aliquid
+        consequatur beatae quod dolore in necessitatibus fugit hic tempora
+        accusamus, magni minus assumenda illum non! Culpa eaque officia totam
+        ipsam, nesciunt sapiente veritatis, non aperiam molestias consequatur
+        dignissimos sint! Repellendus magni, pariatur quibusdam, praesentium
+        molestiae veniam maxime cupiditate qui fuga ducimus sint nostrum id nam
+        eius nisi alias nulla enim consequatur assumenda obcaecati modi
+        architecto soluta perferendis? Cumque facere nobis provident modi
+        veritatis illum nemo similique, necessitatibus veniam dolores fugiat
+        officia! Ullam ea cum adipisci, quae nihil neque illum ducimus nisi.
+        Exercitationem.
+      </p>
+    </div>
+  );
 }
--- 10ProjetsReact/10ProjetsReact/4.React-animations/src/Pages/StateAnim/StateAnim.tsx
+++ 10ProjetsReact/10ProjetsReact/4.React-animations/src/Pages/StateAnim/StateAnim.tsx
@@ -1,42 +1,67 @@
-import { useState, useRef, useEffect, MutableRefObject, RefObject, FormEvent } from "react";
+import { useState, useRef, RefObject, useEffect, FormEvent } from "react";
 import "./StateAnim.css";
-import { useTransition, animated } from "react-spring";
+// import { useTransition, animated } from "react-spring";
 import { v4 as uuidv4 } from "uuid";
+import { animated, useTransition } from "react-spring";
+
+const initialState = [
+  {
+    id: uuidv4(),
+    txt: "Chopin",
+  },
+  {
+    id: uuidv4(),
+    txt: "Mozart",
+  },
+  {
+    id: uuidv4(),
+    txt: "Bach",
+  },
+];
 
 export default function StateAnim() {
-  const [inputData, setInputData] = useState([
-    {
-      id: uuidv4(),
-      txt: "Chopin",
-    },
-    {
-      id: uuidv4(),
-      txt: "Mozart",
-    },
-    {
-      id: uuidv4(),
-      txt: "Bach",
-    },
-  ]);
+  const [inputData, setInputData] = useState(initialState);
+  const [firstDisplay, setFirstDisplay] = useState(true);
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setFirstDisplay(false);
+    }, 1000);
+  }, []);
 
-const inputRef : RefObject<HTMLInputElement> = useRef(null);
-const handleData = (e : FormEvent) => {
-  e.preventDefault();
-  const newObj = {
-    id: uuidv4(),
-    txt: inputRef.current!.value
-  }
-}
+  const inputRef: RefObject<HTMLInputElement> = useRef(null);
 
-  return <form>
-    <label htmlFor="piano">Renseignez vos pianistes favoris</label>
-    <input type="text" id="piano" ref={inputRef}/>
-    <ul>
-      {
-        inputData.map((item) => (
-          <li key={item.id}>{item.txt}</li>
-        ))
-      }
+  const handleData = (e: FormEvent) => {
+    e.preventDefault();
+    setInputData([
+      ...inputData,
+      { id: uuidv4(), txt: inputRef.current!.value },
+    ]);
+    inputRef.current!.value = "";
+  };
+
+  const listTransitions = useTransition(inputData, {
+    from: { opacity: 0, transform: "translateY(10px)" },
+    enter: { opacity: 1, transform: "translateY(0px)" },
+    keys: inputData.map((item) => item.id),
+  });
+
+  return (
+    <form onSubmit={handleData}>
+      <label htmlFor="piano">Renseignez vos pianistes favoris</label>
+      <input
+        type="text"
+        id="piano"
+        placeholder="Saisir un pianiste..."
+        ref={inputRef}
+      />
+      <ul>
+        {firstDisplay
+          ? inputData.map((item) => <li key={item.id}>{item.txt}</li>)
+          : listTransitions((styles, item) => (
+              <animated.li style={styles}>{item.txt}</animated.li>
+            ))}
       </ul>
-  </form>;
+    </form>
+  );
 }
